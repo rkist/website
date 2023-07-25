6a294b43ac1b62bf8d26bb1e03ced47cbc227305
Raul Kist <raulkist@gmail.com>
2023-07-25 13:57:08 -0300

    wow!

--- app/gpthack/routes.py
+++ app/gpthack/routes.py
@@ -5,8 +5,15 @@ from chatgpt.gepeto import create_gpthack
 # Create a Blueprint instance for the routes
 bp = Blueprint('gpthack_routes', __name__)
 
+gpthack_routes_prefix = "/hack"
+
+def fix_pathes(html: str):
+    html = html.replace("href=\"/", "href=\"/hack/")
+    return html
+
 @bp.route('/hack/<path:path>', methods=['GET'])
 def api_get(path):
-    aaa = create_gpthack()
-    return f"<p>hack: {path}</p>"
+    print(f"Path: {path}")
+    html = fix_pathes(create_gpthack(path))
+    return html
 
